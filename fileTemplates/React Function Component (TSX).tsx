4e7a9bdba73787df00ebc27fe00b7243b8ced352
fredrik <fredrik@localhost>
2025-01-14 09:54:17 +0100

    PS-2024.1.4 <fredrik@MacBook-Pro-som-tillhor-Fredrik.local Create React Function Component (TSX).tsx

---
+++ fileTemplates/React Function Component (TSX).tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '@/components/Text';
+
+interface ${NAME}Props extends React.ComponentProps<typeof View> {
+  //
+}
+
+export default function $NAME(props: ${NAME}Props) {
+  return (
+    <View className={""}><Text></Text></View>
+  )
+}

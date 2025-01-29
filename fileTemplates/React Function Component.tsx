e78e6708f4a853e63beb02c34dadfb6f29c3be62
fredrik <fredrik@localhost>
2025-01-29 11:19:39 +0100

    PS-2024.1.4 <fredrik@MacBook-Pro-som-tillhor-Fredrik.local Create React Function Component.tsx

---
+++ fileTemplates/React Function Component.tsx
@@ -0,0 +1,11 @@
+import React, { ComponentProps } from 'react';
+
+interface ${NAME}Props extends ComponentProps<any> {
+  //
+}
+
+export default function $NAME(props: ${NAME}Props) {
+  return (
+    <></>
+  )
+}
